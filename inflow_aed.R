5e27e12337673a650d0609fe4b955e06be939092
Austin Delany <addelany@gmail.com>
2024-05-10 13:49:47 -0500

    Update inflow_aed.R

--- inflow_aed.R
+++ inflow_aed.R
@@ -6,7 +6,7 @@ inflow_targets_file <- "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/
 met_target_file <- "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-met-targets.csv.gz"
 
 horizon <- 34
-reference_datetime <- lubridate::as_date("2024-05-09") 
+reference_datetime <- lubridate::as_date("2024-05-10") 
 #reference_datetime <- Sys.Date()
 noaa_date <- reference_datetime - lubridate::days(1)
 ensemble_members <- 31
