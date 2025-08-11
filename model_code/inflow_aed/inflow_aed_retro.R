25eae1c4a836c8955eb55d1591dbadf718e8e1fb
Austin Delany <addelany@gmail.com>
2025-08-11 11:02:03 -0500

    Update inflow_aed_retro.R

--- model_code/inflow_aed/inflow_aed_retro.R
+++ model_code/inflow_aed/inflow_aed_retro.R
@@ -19,7 +19,7 @@ today <- paste(Sys.Date() - days(2), '00:00:00')
 this_year <- data.frame(date = as.Date(seq.Date(as_date('2025-01-01'), to = as_date(today), by = 'day')))
 
 # check inflow forecast dates
-s3 <- arrow::s3_bucket(bucket = glue::glue("bio230121-bucket01/vera4cast/forecasts/parquet/project_id=vera4cast/duration=P1D/variable=Temp_C_mean/model_id=inflow_gefsClimAED"),
+s3 <- arrow::s3_bucket(bucket = glue::glue("bio230121-bucket01/vera4cast/forecasts/archive-parquet/project_id=vera4cast/duration=P1D/variable=Temp_C_mean/model_id=inflow_gefsClimAED"),
                        endpoint_override = "https://amnh1.osn.mghpcc.org",
                        anonymous = TRUE)
 
