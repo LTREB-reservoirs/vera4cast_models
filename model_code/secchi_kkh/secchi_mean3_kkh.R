0a889d0c19edcf0b5e8de9eb8b3302cd5bb039f7
Austin Delany <addelany@gmail.com>
2026-04-13 09:42:01 -0500

    Save forecast output to CSV and validate file

--- model_code/secchi_kkh/secchi_mean3_kkh.R
+++ model_code/secchi_kkh/secchi_mean3_kkh.R
@@ -9,6 +9,8 @@ forecast_output <- secchi_forecast(forecast_start = Sys.Date(),
                                    forecast_variable = "Secchi_m_sample",
                                    targets_url = "https://amnh1.osn.mghpcc.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz")
 
-vera4castHelpers::forecast_output_validator(forecast_output)
+write.csv(forecast_output, './model_output/secchi_kkh/secchi_kkh.csv')
+
+vera4castHelpers::forecast_output_validator('./model_output/secchi_kkh/secchi_kkh.csv')
 
 vera4castHelpers::submit(forecast_output, s3_region = "submit", s3_endpoint = "ltreb-reservoirs.org", first_submission = FALSE)
